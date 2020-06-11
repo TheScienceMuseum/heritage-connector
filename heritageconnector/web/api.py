5afe37493d85b5668e63b5136fcd02dda18a8768
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-06-11 10:55:34 +0100

    run pre-commit hooks retroactively and allow AWS missing credentials

--- heritageconnector/web/api.py
+++ heritageconnector/web/api.py
@@ -17,7 +17,7 @@ nlp = spacy.load("en_core_web_sm")
 
 
 def fetchandscan(id):
-    page = requests.get(f'https://collection.sciencemuseumgroup.org.uk/objects/{id}')
+    page = requests.get(f"https://collection.sciencemuseumgroup.org.uk/objects/{id}")
     tree = html.fromstring(page.content)
     description = tree.xpath('//div[@class="columns medium-8"]/p//text()')[0].strip()
     app.logger.info(description)
@@ -25,16 +25,16 @@ def fetchandscan(id):
     return displacy.render(doc, style="ent", minify=True)
 
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route('/scan')
+@app.route("/scan")
 def scan():
-    id = request.args.get('id')
+    id = request.args.get("id")
     return fetchandscan(id)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
     packages=setuptools.find_packages(),
 )
