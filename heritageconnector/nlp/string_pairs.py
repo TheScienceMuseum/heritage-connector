fa304b1912818756c4953fa51469851948e8e3c6
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-06-22 14:34:54 +0100

    fuzzy string matcher

---
+++ heritageconnector/nlp/string_pairs.py
@@ -0,0 +1,21 @@
+from fuzzywuzzy import fuzz
+
+
+def fuzzy_match(
+    string1: str, string2: str, scorer=fuzz.token_sort_ratio, threshold=90
+) -> bool:
+    """
+    Work out whether two strings are similar to each other.
+
+    Args:
+        string1 (str), string2(str): the two strings to compare
+        scorer (func): a function which takes two strings and returns a similarity measure between 0 and 100
+        threshold (int): a threshold above which two strings are considered similar. Between 0 and 100.
+
+    Returns:
+        bool: True if strings are close matches
+    """
+
+    assert 0 <= threshold <= 100
+
+    return scorer(string1, string2)
