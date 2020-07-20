cd318f0cff037b328faff44ea6c9835b562e9ad7
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-07-20 14:10:57 +0100

    app takes lightweight sparql queries

---
+++ heritageconnector/web/app.py
@@ -0,0 +1,33 @@
+import uvicorn
+from fastapi import FastAPI
+from heritageconnector import datastore
+import logging
+import os
+
+app = FastAPI()
+logger = logging.getLogger("heritageconnector.app")
+
+# application data
+data = {}
+
+
+@app.on_event("startup")
+def startup():
+    data["graph"] = datastore.es_to_rdflib_graph()
+    logger.info(f"{len(data['graph'])} triples loaded into RDF store")
+
+
+@app.get("/query")
+def query(sparql: str):
+    res = data["graph"].query(sparql)
+
+    if "construct" in sparql.lower():
+        return [{"subject": i[0], "predicate": i[1], "object": i[2]} for i in res]
+    else:
+        # e.g. SELECT
+        return [i for i in res]
+
+
+if __name__ == "__main__":
+    logger.setLevel(logging.DEBUG)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
