7f0cbbc053777afa4e9e645f8782823edb5b44c4
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-07-22 09:49:18 +0100

    enable eslint; FDGraph to HTML

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2020": true
+    },
+    "plugins": ["html"],
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 11,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
