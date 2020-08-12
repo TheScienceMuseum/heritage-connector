a17f48880ef15ec18bea154527fdcdf3a659e44d
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-12 13:57:27 +0100

    csv/nt converter update

--- smg_jobs/es_to_csv.py
+++ smg_jobs/es_to_csv.py
@@ -5,23 +5,29 @@ sys.path.append("..")
 from heritageconnector.datastore import es_to_rdflib_graph
 import csv
 
-if len(sys.argv) == 1:
-    raise ValueError("filename must be provided as argument")
 
-file_path = sys.argv[1]
+if len(sys.argv) == 1:
+    raise ValueError(
+        "output format (csv/ntriples) and filename must be provided as arguments"
+    )
+if len(sys.argv) == 2:
+    raise ValueError("missing either output format or filename")
+
+method = sys.argv[1]
+file_path = sys.argv[2]
 
 g = es_to_rdflib_graph()
 
-res = g.query(
+if method == "csv":
+    res = g.query(
+        """
+    SELECT ?s ?p ?o WHERE {?s ?p ?o}
     """
-SELECT ?s ?p ?o WHERE {?s ?p ?o}
-"""
-)
+    )
 
-# to csv
-# with open(file_path, "w", newline="") as f:
-#     writer = csv.writer(f, delimiter="\t", quotechar='"')
-#     writer.writerows(res)
+    with open(file_path, "w", newline="") as f:
+        writer = csv.writer(f, delimiter="\t", quotechar='"')
+        writer.writerows(res)
 
-# to ntriples
-g.serialize(file_path, format="ntriples")
+elif method in ["ntriples", "nt"]:
+    g.serialize(file_path, format="ntriples")
