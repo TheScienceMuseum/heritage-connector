71d20d5a1cd0fb94f7319ce085d7c5352fe8ab11
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-13 13:40:06 +0100

    update train script

--- experiments/graph embeddings/dgl-ke/scripts/train.sh
+++ experiments/graph embeddings/dgl-ke/scripts/train.sh
@@ -1,10 +1,11 @@
 DATA_PATH=~/data
 SAVE_PATH=~/data/results
 LOG_INTERVAL=100
+FILE_NAME=train.txt
 
 BATCH_SIZE=1000
 BATCH_SIZE_EVAL=16
 NEG_SAMPLE_SIZE=200
 
-dglke_train --model_name TransE --data_path $DATA_PATH --save_path $SAVE_PATH --batch_size $BATCH_SIZE --log_interval $LOG_INTERVAL \
+dglke_train --model_name TransE --data_path $DATA_PATH --data_files [$FILE_NAME] --gpu 0 --save_path $SAVE_PATH --batch_size $BATCH_SIZE --log_interval $LOG_INTERVAL \
 --batch_size_eval $BATCH_SIZE_EVAL --neg_sample_size $NEG_SAMPLE_SIZE --test --mix_cpu_gpu --async_update
