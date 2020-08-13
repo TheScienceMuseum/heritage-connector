652948213817b4e86400515b059ba03d22a775e5
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-13 14:16:38 +0100

    rm delimiter

--- experiments/graph embeddings/dgl-ke/scripts/train.sh
+++ experiments/graph embeddings/dgl-ke/scripts/train.sh
@@ -4,13 +4,12 @@ DATASET=hc1208
 
 FILE_NAME=train.txt
 FORMAT=raw_udd_htr
-DELIMITER=\t
 
 LOG_INTERVAL=1000
 BATCH_SIZE=1000
 BATCH_SIZE_EVAL=16
 NEG_SAMPLE_SIZE=200
 
-dglke_train --model_name TransE --dataset $DATASET --data_path $DATA_PATH --format FORMAT --data_files [$FILE_NAME] --delimiter $DELIMITER \
+dglke_train --model_name TransE --dataset $DATASET --data_path $DATA_PATH --format FORMAT --data_files [$FILE_NAME] \
 --save_path $SAVE_PATH --batch_size $BATCH_SIZE --log_interval $LOG_INTERVAL --batch_size_eval $BATCH_SIZE_EVAL --neg_sample_size $NEG_SAMPLE_SIZE \
 --gpu 0 --test --mix_cpu_gpu --async_update
