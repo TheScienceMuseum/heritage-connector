aa410397c848123f1badfc7a8d4330a9eea5c4f9
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-13 14:12:12 +0100

    fix script alongside dglke docs

--- experiments/graph embeddings/dgl-ke/scripts/train.sh
+++ experiments/graph embeddings/dgl-ke/scripts/train.sh
@@ -1,12 +1,16 @@
 DATA_PATH=~/data
 SAVE_PATH=~/data/results
-LOG_INTERVAL=1000
-FILE_NAME=train.txt
-FORMAT=raw_udd_{htr}
+DATASET=hc1208
 
+FILE_NAME=train.txt
+FORMAT=raw_udd_htr
+DELIMITER=\t
+
+LOG_INTERVAL=1000
 BATCH_SIZE=1000
 BATCH_SIZE_EVAL=16
 NEG_SAMPLE_SIZE=200
 
-dglke_train --model_name TransE --data_path $DATA_PATH --data_files [$FILE_NAME] --gpu 0 --save_path $SAVE_PATH --batch_size $BATCH_SIZE --log_interval $LOG_INTERVAL \
---batch_size_eval $BATCH_SIZE_EVAL --neg_sample_size $NEG_SAMPLE_SIZE --test --mix_cpu_gpu --async_update
+dglke_train --model_name TransE --dataset $DATASET --data_path $DATA_PATH --format FORMAT --data_files [$FILE_NAME] --delimiter $DELIMITER \
+--save_path $SAVE_PATH --batch_size $BATCH_SIZE --log_interval $LOG_INTERVAL --batch_size_eval $BATCH_SIZE_EVAL --neg_sample_size $NEG_SAMPLE_SIZE \
+--gpu 0 --test --mix_cpu_gpu --async_update
