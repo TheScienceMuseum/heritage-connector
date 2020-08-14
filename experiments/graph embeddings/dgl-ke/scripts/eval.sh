24abd47eaf3517c4328b16155f32fd3a5b5c9f03
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-08-14 15:53:58 +0100

    fix bash scripts

--- experiments/graph embeddings/dgl-ke/scripts/eval.sh
+++ experiments/graph embeddings/dgl-ke/scripts/eval.sh
@@ -9,6 +9,6 @@ LOG_INTERVAL=1000
 BATCH_SIZE_EVAL=16
 NEG_SAMPLE_SIZE=200
 
-dglke_eval --model_name TransE --dataset $DATASET --data_path $DATA_PATH --format FORMAT --data_files train.txt valid.txt test.txt \
+dglke_eval --model_name TransE --dataset $DATASET --data_path $DATA_PATH --format $FORMAT --data_files train.txt valid.txt test.txt \
 --model_path $SAVE_PATH --batch_size_eval $BATCH_SIZE_EVAL --neg_sample_size_eval $NEG_SAMPLE_SIZE \
 --gpu 0 --mix_cpu_gpu 
--- experiments/graph embeddings/dgl-ke/scripts/train.sh
+++ experiments/graph embeddings/dgl-ke/scripts/train.sh
@@ -10,6 +10,6 @@ BATCH_SIZE=1000
 BATCH_SIZE_EVAL=16
 NEG_SAMPLE_SIZE=200
 
-dglke_train --model_name TransE --dataset $DATASET --data_path $DATA_PATH --format FORMAT --data_files [$FILE_NAME] \
+dglke_train --model_name TransE --dataset $DATASET --data_path $DATA_PATH --format $FORMAT --data_files train.txt valid.txt test.txt \
 --save_path $SAVE_PATH --batch_size $BATCH_SIZE --log_interval $LOG_INTERVAL --batch_size_eval $BATCH_SIZE_EVAL --neg_sample_size $NEG_SAMPLE_SIZE \
 --gpu 0 --test --mix_cpu_gpu --async_update
