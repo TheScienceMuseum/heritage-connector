cbd173c74637a16817533bf4286e820505fe3bc9
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-09-18 16:07:33 +0100

    update entity distance cache & upload disambiguation debug template

--- smg_jobs/disambiguation.py
+++ smg_jobs/disambiguation.py
@@ -14,7 +14,7 @@ from heritageconnector.disambiguation.pipelines import build_training_data
 @click.option("--table_name", "-t", type=str)
 @click.option("--limit", "-l", type=int, default=None)
 def make_training_data(
-    output_folder, table_name, limit, page_size=250, search_limit=20
+    output_folder, table_name, limit, page_size=100, search_limit=20
 ):
     X, y, pid_labels, id_pairs = build_training_data(
         table_name, page_size=page_size, search_limit=search_limit, limit=limit
@@ -33,4 +33,7 @@ def make_training_data(
 
 if __name__ == "__main__":
     make_training_data()
-    # make_training_data(".", "PERSON", None)
+    # DEBUG:
+    # make_training_data(
+    #     "/Volumes/Kalyan_SSD/SMG/disambiguation/types_test", "PERSON", limit=None, page_size=100, search_limit=20
+    # )
