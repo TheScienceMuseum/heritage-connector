ae79b082917a08d940ec1868582a51d5881a903f
Kalyan Dutia <kalyan.dutia@gmail.com>
2020-10-16 16:48:17 +0100

    docs: remove bibtex extension

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ links = {
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinxcontrib.bibtex",
+    # "sphinxcontrib.bibtex",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
