7bb5dde306bb0d01d96545ca413b084e2a264584
Kalyan Dutia <kalyan.dutia@gmail.com>
2021-06-15 09:49:16 +0100

    BLINK runner uses disambiguatingDescription and saves timestamp

--- smg_jobs/run_blink.py
+++ smg_jobs/run_blink.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append("..")
 
 from heritageconnector.nlp.nel import BLINKServiceWrapper
+from heritageconnector.utils.generic import get_timestamp
 
 if __name__ == "__main__":
     base_url = sys.argv[1]
@@ -24,11 +25,13 @@ if __name__ == "__main__":
 
     blink_service = BLINKServiceWrapper(
         endpoint,
-        description_field="data.http://www.w3.org/2001/XMLSchema#description",
+        description_field="data.https://schema.org/disambiguatingDescription",
         entity_fields=entity_fields,
         wiki_link_threshold=threshold,
     )
 
     blink_service.process_unlinked_entity_mentions(
-        "../GITIGNORE_DATA/blink_output.jsonl", page_size=12, limit=None
+        f"../GITIGNORE_DATA/blink_output_{get_timestamp()}.jsonl",
+        page_size=12,
+        limit=None,
     )
