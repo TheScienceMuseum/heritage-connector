69f63e6b705a859f00f35d128efb80953a115532
Kalyan Dutia <kalyan.dutia@gmail.com>
2021-06-15 11:20:49 +0100

    defaults for config.ini

--- heritageconnector/config.py
+++ heritageconnector/config.py
@@ -9,22 +9,22 @@ logger = logging.get_logger(__name__)
 
 
 class LoadConfig:
-    def __init__(self, file_name):
-        parser = ConfigParser()
-        parser.optionxform = str  # make option names case sensitive
-        found = parser.read(file_name)
-        if not found:
+    def __init__(self, file_name: str):
+        self.parser = ConfigParser()
+        self.parser.optionxform = str  # make option names case sensitive
+        self._load_config_from_file(
+            os.path.join(this_path, "config.defaults.ini"), defaults=True
+        )
+        self._load_config_from_file(file_name)
+
+    def _load_config_from_file(self, file_name: str, defaults: bool = False):
+        found = self.parser.read(file_name)
+        if not found and not defaults:
             logger.error(
-                "No config file found. Ensure there is a file called `config.ini` in the config folder in the root of this repo. See `config/config.sample.ini` for an example."
+                f"No config file found at {file_name}. Add one to use your own values. See an example at `config/config.sample.ini`."
             )
-        for section in parser.sections():
-            # if in the LOCALPATH section of config, resolve path relative to config
-            if section == "LOCALPATH":
-                config_items = [
-                    (i[0], os.path.join(this_path, i[1])) for i in parser.items(section)
-                ]
-            else:
-                config_items = parser.items(section)
+        for section in self.parser.sections():
+            config_items = self.parser.items(section)
 
             self.__dict__.update(config_items)
 
